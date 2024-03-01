851feb08eeb46e4052e5440db819a315fbf198af
bt-koch <bela.koch@hotmail.com>
2024-03-01 16:37:29 +0100

    convert user input to data.frame

--- R/datahandling.R
+++ R/datahandling.R
@@ -2,3 +2,18 @@ read_data <- function() {
   df <- read.csv("data/lgd_dataset.csv")
   return(df)
 }
+
+read_input <- function(input) {
+  
+  df <- data.frame(
+    customer = input$customer_type,
+    real.estate.type = input$realestate_type,
+    loan.amount = input$loan_amount,
+    mortgage.collateral.MV = input$mortgage_collateral_mv,
+    additional.collateral.MV = input$additional_collateral_mv,
+    additional.collateral.type = input$additional_collateral_type
+  )
+  
+  return(df)
+  
+}
