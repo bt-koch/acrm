4365a8d551f082a17fa6bf28a6febceef2940594
bt-koch <bela.koch@hotmail.com>
2024-03-01 15:52:06 +0100

    add all predictors as user input

---
+++ ui.R
@@ -0,0 +1,51 @@
+# Define UI
+ui <- fluidPage(
+  
+  # App title
+  titlePanel("MVP: LGD-Modeling"),
+  
+  # Sidebar layout
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "customer_type",
+        label = "Select customer type",
+        choices = read_data()$customer |> unique()
+      ),
+      selectInput(
+        inputId = "realestate_type",
+        label = "Select type of real estate",
+        choices = read_data()$real.estate.type |> unique()
+      ),
+      numericInput(
+        inputId = "loan_amount",
+        label = "Loan amount to customer",
+        value = NA,
+        min = 0
+      ),
+      numericInput(
+        inputId = "mortgage_collateral_mv",
+        label = "Market value of mortgage collateral",
+        value = NA,
+        min = 0
+      ),
+      numericInput(
+        inputId = "additional_collateral_mv",
+        label = "Market value of additional collateral",
+        value = NA,
+        min = 0
+      ),
+      selectInput(
+        inputId = "additional_collateral_type",
+        label = "Type of additional collateral",
+        choices = read_data()$additional.collateral.type |> unique()
+      ),
+      actionButton("calculate", "Calculate")
+    ),
+    
+    # Main panel layout
+    mainPanel(
+      textOutput("printedText")
+    )
+  )
+)
