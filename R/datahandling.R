b257c4b55bdf28f36b427883920849f807f33f1b
bt-koch <bela.koch@hotmail.com>
2024-03-02 08:13:13 +0100

    single function to prepare model input

--- R/datahandling.R
+++ R/datahandling.R
@@ -3,8 +3,18 @@ read_data <- function() {
   return(df)
 }
 
+prepare_data <- function(data = read_data()) {
+  # TODO: Problem -> we have collinearity
+  data$corporate <- ifelse(data$customer == "corporate", 1, 0)
+  data$appartment <- ifelse(data$real.estate.type == "appartment", 1, 0)
+  data$office_building <- ifelse(data$real.estate.type == "office building", 1, 0)
+  data$retirement_account <- ifelse(data$additional.collateral.type == "retirement account", 1, 0)
+  data$cash_account <- ifelse(data$additional.collateral.type == "cash account", 1, 0)
+  data$X <- data$customer <- data$real.estate.type <- data$additional.collateral.type <- NULL
+  return(data)
+}
+
 read_input <- function(input) {
-  
   df <- data.frame(
     customer = input$customer_type,
     real.estate.type = input$realestate_type,
@@ -13,7 +23,5 @@ read_input <- function(input) {
     additional.collateral.MV = input$additional_collateral_mv,
     additional.collateral.type = input$additional_collateral_type
   )
-  
   return(df)
-  
 }
