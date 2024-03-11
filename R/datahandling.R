2130452f52f0170d97366de9aa5548137b656623
bt-koch <bela.koch@hotmail.com>
2024-03-11 11:23:38 +0100

    segmentize data | get relevant segment

--- R/datahandling.R
+++ R/datahandling.R
@@ -4,20 +4,38 @@ read_data <- function() {
 }
 
 prepare_data <- function(data = read_data()) {
-  # TODO: Problem -> we have collinearity
-  data$corporate <- ifelse(data$customer == "corporate", 1, 0)
+
   data$appartment <- ifelse(data$real.estate.type == "appartment", 1, 0)
   data$office_building <- ifelse(data$real.estate.type == "office building", 1, 0)
   data$retirement_account <- ifelse(data$additional.collateral.type == "retirement account", 1, 0)
   data$cash_account <- ifelse(data$additional.collateral.type == "cash account", 1, 0)
-  data$X <- data$customer <- data$real.estate.type <- data$additional.collateral.type <- NULL
-  return(data)
+  
+  data$X <- data$real.estate.type <- data$additional.collateral.type <- NULL
+  names(data) <- gsub("\\.", "_", names(data)) |> tolower()
+  
+  data$lgd <- data$lgd * data$loan_amount
+  
+  segment_1 <- data[data$customer == "private",]
+  segment_2 <- data[data$customer == "corporate",]
+  segment_1$customer <- NULL
+  segment_2$customer <- NULL
+  
+  return(list("segment_1" = segment_1, "segment_2" = segment_2))
+}
+
+get_relevant_segment <- function(input) {
+  df_input <- read_input(input)
+  if (unique(df_input$customer_type) == "private") {
+    return("segment_1")
+  } else if (unique(df_input$customer_type == "corporate")) {
+    return("segment_2")
+  }
 }
 
 read_input <- function(input) {
   df <- data.frame(
     customer = input$customer_type,
-    real.estate.type = input$realestate_type,
+    real.estate.type = input$real_estate_type,
     loan.amount = input$loan_amount,
     mortgage.collateral.MV = input$mortgage_collateral_mv,
     additional.collateral.MV = input$additional_collateral_mv,
@@ -25,6 +43,7 @@ read_input <- function(input) {
   )
   return(df)
 }
+
 
 render_value <- function(value, type = "percent") {
   if (type == "percent") {
