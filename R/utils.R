c85b50c80772d33cb4f352677a1dbf7b2342e895
bt-koch <bela.koch@hotmail.com>
2024-03-11 10:54:18 +0100

    remove all objects | prepare session for testing app

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,13 @@ load_all <- function() {
     source(script)
   }
 }
+
+reset_workspace <- function() {
+  rmv_objects <- ls(envir = .GlobalEnv)[sapply(ls(envir = .GlobalEnv), function(i) class(get(i))) != "function"]
+  rm(list = rmv_objects, pos = .GlobalEnv)
+}
+
+prepare_session <- function() {
+  load_all()
+  reset_workspace()
+}
